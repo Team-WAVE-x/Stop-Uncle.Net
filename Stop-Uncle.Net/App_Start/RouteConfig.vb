4b9837fde60b53fca0051c0959c96f401f0b2a2f
Kate Lin <63230494+KateLin-BASIC@users.noreply.github.com>
2020-10-08 15:50:31 +0900

    Add files via upload

---
+++ Stop-Uncle.Net/App_Start/RouteConfig.vb
@@ -0,0 +1,18 @@
+﻿Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Web
+Imports System.Web.Mvc
+Imports System.Web.Routing
+
+Public Module RouteConfig
+    Public Sub RegisterRoutes(ByVal routes As RouteCollection)
+        routes.IgnoreRoute("{resource}.axd/{*pathInfo}")
+
+        routes.MapRoute(
+            name:="Aje",
+            url:="api/{type}",
+            defaults:=New With {.controller = "Aje", .action = "Index", .type = "plain"}
+        )
+    End Sub
+End Module
---
+++ Stop-Uncle.Net/Global.asax.vb
@@ -0,0 +1,12 @@
+﻿Imports System.Web.Mvc
+Imports System.Web.Routing
+
+Public Class Global_asax
+    Inherits HttpApplication
+
+    Sub Application_Start(sender As Object, e As EventArgs)
+        ' 애플리케이션 시작 시 발생
+        AreaRegistration.RegisterAllAreas()
+        RouteConfig.RegisterRoutes(RouteTable.Routes)
+    End Sub
+End Class
